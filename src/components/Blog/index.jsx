87db14fc6c3ef1d12933d40aa86420c55c5b9f53
skaroweb <52270169+skaroweb@users.noreply.github.com>
2023-10-19 11:57:49 +0530

    fixes

--- src/components/Blog/index.jsx
+++ src/components/Blog/index.jsx
@@ -74,6 +74,16 @@ const BlogList = () => {
     return title;
   }
 
+  // Create a function to format the date as "YYYY-MM-DD HH:MM"
+  function formatDate2(date) {
+    var year = date.getFullYear();
+    var month = String(date.getMonth() + 1).padStart(2, "0");
+    var day = String(date.getDate()).padStart(2, "0");
+    var hours = String(date.getHours()).padStart(2, "0");
+    var minutes = String(date.getMinutes()).padStart(2, "0");
+
+    return year + "-" + month + "-" + day + " " + hours + ":" + minutes;
+  }
   return (
     <HelmetProvider>
       <div>
@@ -118,7 +128,9 @@ const BlogList = () => {
 
                     <time
                       className="time text-center"
-                      dateTime="2017-02-03 20:00"
+                      dateTime={formatDate2(
+                        new Date(blog.attributes.publishedAt)
+                      )}
                     >
                       {formatDate(blog.attributes.publishedAt).split(" ")[1]}
                       <span className="txt">
